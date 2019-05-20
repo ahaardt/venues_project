5195b44070ebe828832e79f481fd1bbd69361af7
Alexander Haardt <haardt.alexander@gmail.com>
2019-05-20 14:43:27 -0500

    Event

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './resources/styles.css';
 import Header from './components/Header';
 import Featured from './components/featured/';
 import TimeUntil from './components/featured/';
+import VenueInfo from './components/venueinfo';
 
 function App() {
   return (
-    <div className="App" style = {{height:"1500px", background:'cornflowerblue'}}>
+    <div className="App">
 
     <div>
     <Header/>
-    <Featured/>
+
     <TimeUntil/>
+    <VenueInfo/>
     </div>
 
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,88 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
+
 import SideDrawer from './SideDrawer';
 
- class Header extends Component {
+class Header extends Component {
 
     state = {
         drawerOpen: false,
         headerShow: false
     }
 
-
-    componentDidMount () {
+    componentDidMount(){
         window.addEventListener('scroll',this.handleScroll);
     }
 
 
-
     handleScroll = () => {
-        if(window.scrollY > 0) {
-            this.setState ({
-               headerShow: true
+       if(window.scrollY > 0){
+            this.setState({
+                headerShow: true
             })
-        }
-        else {
-            this.setState ({
-               headerShow: false
+       } else {
+            this.setState({
+                headerShow: false
             })
-
-        }
+       }
     }
 
+
     toggleDrawer = (value) => {
-        this.setState ({
+        this.setState({
             drawerOpen: value
         })
     }
 
-
     render() {
         return (
-            <div>
             <AppBar
-                position ="fixed"
-                style = {{
-                    backgroundColor: this.state.headerShow ? '#2f2f2f' : ' transparent',
-                    boxShaodw: 'none',
+                position="fixed"
+                style={{
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
+                    boxShadow: 'none',
                     padding: '10px 0px'
-
-                }}>
-
-
+                }}
+            >
                 <Toolbar>
 
-
-                    <div className ="leader_logo">
-                        <div className ='font_righteous header_logo_venue'> Ragnarok </div>
-                        <div className = 'header_logo_title'> Icelandic Adventures </div>
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">Ragnarok </div>
+                        <div className="header_logo_title"> Icelandic Tours</div>
                     </div>
 
-
-                     <IconButton
-                        style={{float : 'left', paddingRight : '5px'}}
+                    <IconButton
                         aria-label="Menu"
-                        color="inhert"
-                        onClick = {() => this.toggleDrawer(true)}
-                        >
+                        color="inherit"
+                        onClick={()=> this.toggleDrawer(true)}
+                    >
                         <MenuIcon/>
                     </IconButton>
 
-
                     <SideDrawer
-                        open = {this.state.drawerOpen}
-                        // receives false as argument from SideDrawer
-                        onClose = {(value) => this.toggleDrawer(value)}
-
+                        open={this.state.drawerOpen}
+                        onClose={(value)=> this.toggleDrawer(value)}
                     />
+
+
                 </Toolbar>
-
             </AppBar>
-
-            </div>
         );
     }
 }
